84ad2603c398fad72b853e4c1b1677b1f926c1c0
AkioShimono <noaokku@gmail.com>
2020-09-01 15:59:58 +0900

    content.rb

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,5 @@
 class Content < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user
   validates :title, presence: true
   validates :url, presence: true, uniqueness: true
 end
